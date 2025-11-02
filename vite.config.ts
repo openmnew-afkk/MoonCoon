ab88c368cec04a84c410f80725a1361d501d56c7
Sauce <sauce@Sauces-MacBook-Air.local>
2025-11-03 01:32:54 +0300

    fix: add production config without server code references

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Явно указываем что включать в сборку
+  include: ['client/**/*', 'shared/**/*', 'index.html', 'public/**/*'],
   root: ".",
   publicDir: "public",
   server: {
